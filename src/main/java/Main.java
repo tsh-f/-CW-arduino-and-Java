e8c13e0864a63519b2ec56f55ed3ddfd9a1af42b
Tash <nvf325@gmail.com>
2019-12-12 23:13:50 +0400

    add lib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,18 @@
 import arduino.Arduino;
-
-import java.lang.invoke.SerializedLambda;
+import com.fazecast.jSerialComm.SerialPort;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-
-        Scanner scanner = new Scanner(System.in);
-        Arduino arduino = new Arduino("COM52", 9600);
-        boolean connected = arduino.openConnection();
-        System.out.println("Соединение установлено: " + connected);
-        Thread.sleep(5000);
-
-        label_1:
-        while (scanner.hasNext()) {
-
-            String s = scanner.nextLine();
-
-            switch (s) {
-                case "on":
-                    arduino.serialWrite('1');
-                    break;
-                case "off":
-                    arduino.serialWrite('0');
-                    break;
-                case "exit":
-                    arduino.serialWrite('0');
-                    arduino.closeConnection();
-                    break label_1;
-                default:
-                    System.out.println(s + " - не является командой");
-                    break;
-            }
+    public static void main(String[] args) {
+        int tmp = 0;
+        SerialPort[] ports = SerialPort.getCommPorts();
+        System.out.println("Введите номер COM порта: ");
+        Arrays.stream(ports).forEach((x -> System.out.println((tmp + 1) + " " + x.getSystemPortName())));
+        int index = (new Scanner(System.in)).nextInt() - 1;
+        Arduino port = new Arduino(ports[index].getSystemPortName(), 9600);
+        while (port.openConnection()){
+            System.out.println(port.serialRead());
         }
     }
 }
-    }
-            }
