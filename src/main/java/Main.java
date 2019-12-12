e7cf28274c4605fae50f89a144a0b5a281b5f767
Tash <nvf325@gmail.com>
2019-12-13 01:40:50 +0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,28 @@
 import arduino.Arduino;
 import com.fazecast.jSerialComm.SerialPort;
-import java.util.Arrays;
+import gnu.io.CommPort;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int tmp = 0;
         SerialPort[] ports = SerialPort.getCommPorts();
         System.out.println("Введите номер COM порта: ");
-        Arrays.stream(ports).forEach((x -> System.out.println((tmp + 1) + " " + x.getSystemPortName())));
+        for (SerialPort port : ports) {
+            System.out.println(tmp + " " + port.getSystemPortName());
+            tmp++;
+        }
         int index = (new Scanner(System.in)).nextInt() - 1;
         Arduino port = new Arduino(ports[index].getSystemPortName(), 9600);
-        while (port.openConnection()){
-            System.out.println(port.serialRead());
-        }
+        Thread thread = new Thread(() -> {
+            while (port.openConnection()) {
+
+            }
+        });
+        thread.start();
+        thread.join();
+
     }
 }
+
