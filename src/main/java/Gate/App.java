182bae618a799937c072f042b6964485a2a88c26
Tash <nvf325@gmail.com>
2019-12-14 23:57:18 +0400

    add scene

--- src/main/java/Gate/App.java
+++ src/main/java/Gate/App.java
@@ -3,47 +3,82 @@ package Gate;
 import com.fazecast.jSerialComm.SerialPort;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 public class App extends Application {
+    int index;
+    private Semaphore lock;
+    private VBox root;
+    private TextArea text;
+    private TextField field;
+    private Button submit;
+    private Scene scene;
+
+
+    {
+        lock = new Semaphore(1);
+        root = new VBox();
+        text = new TextArea();
+        field = new TextField();
+        submit = new Button("Enter");
+        text.setPrefSize(500, 400);
+        text.setText("Enter COM port number:\n");
+        text.setEditable(false);
+        root.getChildren().addAll(text, field, submit);
+        scene = new Scene(root, 500, 500);
+        submit.setOnAction(e -> {
+            index = Integer.parseInt(field.getText());
+            lock.release();
+        });
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 200,100);
-        TextArea text = new TextArea();
-        text.setText("Введите номер COM порта: ");
+
+        primaryStage.setTitle("Gate");
         primaryStage.setScene(scene);
         primaryStage.show();
-        SerialPort[] ports = SerialPort.getCommPorts();
-        System.out.println("Введите номер COM порта: ");
 
-        for (int i = 0; i < ports.length; i++) {
-            System.out.println(i + " " + ports[i].getSystemPortName());
-        }
+        Thread uiThread = new Thread(() -> {
 
-        int index = (new Scanner(System.in)).nextInt();
-        SerialPort comPort = SerialPort.getCommPort(ports[index].getSystemPortName());
+            try {
+                SerialPort[] ports = SerialPort.getCommPorts();
 
-        comPort.setBaudRate(9600);
-        comPort.setNumDataBits(8);
-        comPort.setParity(SerialPort.NO_PARITY);
-        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
-        comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
-        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
-        comPort.openPort();
+                for (int i = 0; i < ports.length; i++) {
+                    text.appendText(i + " >> " + ports[i].getSystemPortName() + "\n");
+                }
+                lock.acquire();
+//                SerialPort comPort = SerialPort.getCommPort(ports[index].getSystemPortName());
 
-        byte[] buff = new byte[8];
-        long bytes = 8;
+//                comPort.setBaudRate(9600);
+//                comPort.setNumDataBits(8);
+//                comPort.setParity(SerialPort.NO_PARITY);
+//                comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+//                comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+//                comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+//                comPort.openPort();
 
-        comPort.readBytes(buff, bytes);
-        while (comPort.isOpen()) {
-            comPort.readBytes(buff, bytes);
-            System.out.print(new String(buff));
-        }
+//                byte[] buff = new byte[8];
+//                long bytes = 8;
+
+//                comPort.readBytes(buff, bytes);
+//        while (comPort.isOpen()) {
+//            comPort.readBytes(buff, bytes);
+//            System.out.print(new String(buff));
+//        }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        uiThread.start();
+        uiThread.join();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/Gate/Main.java
+++ src/main/java/Gate/Main.java
@@ -3,8 +3,7 @@ package Gate;
 public class Main {
 
     public static void main(String[] args) {
-        App app = new App();
-        app.main(args);
+        App.main(args);
     }
 }
 
