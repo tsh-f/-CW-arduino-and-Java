5d6b421912db9b90f47975dcb57338ad5d315042
Tash <nvf325@gmail.com>
2019-12-15 01:42:39 +0400

    text not append

--- src/main/java/Gate/App.java
+++ src/main/java/Gate/App.java
@@ -9,20 +9,22 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.concurrent.Semaphore;
+import java.util.Scanner;
 
 public class App extends Application {
     int index;
-    private Semaphore lock;
     private VBox root;
     private TextArea text;
     private TextField field;
     private Button submit;
     private Scene scene;
+    private byte[] buff;
+    private long bytes;
+    private SerialPort comPort;
+    private SerialPort[] ports;
 
 
     {
-        lock = new Semaphore(1);
         root = new VBox();
         text = new TextArea();
         field = new TextField();
@@ -32,54 +34,99 @@ public class App extends Application {
         text.setEditable(false);
         root.getChildren().addAll(text, field, submit);
         scene = new Scene(root, 500, 500);
+
+
         submit.setOnAction(e -> {
-            index = Integer.parseInt(field.getText());
-            lock.release();
+            buttonAction();
+            readBytesOfCOMPort();
         });
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         primaryStage.setTitle("Gate");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        Thread uiThread = new Thread(() -> {
+        ports = SerialPort.getCommPorts();
 
-            try {
-                SerialPort[] ports = SerialPort.getCommPorts();
+        for (int i = 0; i < ports.length; i++) {
+            text.appendText(i + " >> " + ports[i].getSystemPortName() + "\n");
+        }
+    }
 
-                for (int i = 0; i < ports.length; i++) {
-                    text.appendText(i + " >> " + ports[i].getSystemPortName() + "\n");
-                }
-                lock.acquire();
-//                SerialPort comPort = SerialPort.getCommPort(ports[index].getSystemPortName());
+    private void buttonAction(){
+        field.setVisible(false);
+        submit.setVisible(false);
+        text.setText("");
+        index = Integer.parseInt(field.getText());
+        comPort = SerialPort.getCommPort(ports[index].getSystemPortName());
 
-//                comPort.setBaudRate(9600);
-//                comPort.setNumDataBits(8);
-//                comPort.setParity(SerialPort.NO_PARITY);
-//                comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
-//                comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
-//                comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
-//                comPort.openPort();
+        comPort.openPort();
+        comPort.setBaudRate(9600);
+        comPort.setNumDataBits(8);
+        comPort.setParity(SerialPort.NO_PARITY);
+        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+    }
 
-//                byte[] buff = new byte[8];
-//                long bytes = 8;
-
-//                comPort.readBytes(buff, bytes);
+    private void readBytesOfCOMPort() {
+        buff = new byte[13];
+        bytes = 13;
+        String tmp;
+        Scanner scanner = new Scanner(comPort.getInputStream());
+        while(comPort.isOpen()){
+            if(scanner.hasNextLine()){
+                System.out.println(scanner.nextLine());
+            }
+        }
+//        comPort.readBytes(buff, bytes);
 //        while (comPort.isOpen()) {
 //            comPort.readBytes(buff, bytes);
-//            System.out.print(new String(buff));
+//            text.appendText(new String(buff));
 //        }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        uiThread.start();
-        uiThread.join();
-
     }
+
+//    class MainUI implements Runnable {
+//
+//        @Override
+//        public void run() {
+//
+//                ports = SerialPort.getCommPorts();
+//
+//                for (int i = 0; i < ports.length; i++) {
+//                    text.appendText(i + " >> " + ports[i].getSystemPortName() + "\n");
+//                }
+//
+//                buff = new byte[8];
+//                bytes = 8;
+////                comPort.readBytes(buff, bytes);
+////                while (comPort.isOpen()) {
+////                    comPort.readBytes(buff, bytes);
+////                    text.appendText(new String(buff));
+////                }
+//
+//        }
+//    }
+
+//    class ReadBytesOfCOMPort implements Runnable {
+//
+//        @Override
+//        public void run() {
+//            try {
+//                lock.acquire();
+//                comPort.readBytes(buff, bytes);
+//                while (comPort.isOpen()) {
+//                    comPort.readBytes(buff, bytes);
+//                    text.appendText(new String(buff));
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
 
     public static void main(String[] args) {
         launch(args);
--- src/main/java/Gate/Main.java
+++ src/main/java/Gate/Main.java
@@ -2,7 +2,7 @@ package Gate;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         App.main(args);
     }
 }
