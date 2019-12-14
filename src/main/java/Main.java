4fc3cb6aa4012a97fb58899e8dbec4d2ac8532a7
Tash <nvf325@gmail.com>
2019-12-14 14:28:31 +0400

    eureka!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,36 @@
-import arduino.Arduino;
 import com.fazecast.jSerialComm.SerialPort;
-import gnu.io.CommPort;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        int tmp = 0;
+    public static void main(String[] args) throws IOException {
         SerialPort[] ports = SerialPort.getCommPorts();
         System.out.println("Введите номер COM порта: ");
-        for (SerialPort port : ports) {
-            System.out.println(tmp + " " + port.getSystemPortName());
-            tmp++;
-        }
-        int index = (new Scanner(System.in)).nextInt() - 1;
-        Arduino port = new Arduino(ports[index].getSystemPortName(), 9600);
-        Thread thread = new Thread(() -> {
-            while (port.openConnection()) {
 
-            }
-        });
-        thread.start();
-        thread.join();
+        for (int i = 0; i < ports.length; i++) {
+            System.out.println(i + " " + ports[i].getSystemPortName());
+        }
+
+        int index = (new Scanner(System.in)).nextInt();
+        SerialPort comPort = SerialPort.getCommPort(ports[index].getSystemPortName());
+
+        comPort.setBaudRate(9600);
+        comPort.setNumDataBits(8);
+        comPort.setParity(SerialPort.NO_PARITY);
+        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+        comPort.openPort();
+
+        byte[] buff = new byte[8];
+        long bytes = 8;
+
+        while(comPort.isOpen()){
+            comPort.readBytes(buff, bytes);
+            System.out.print(new String(buff));
+        }
 
     }
 }
-
